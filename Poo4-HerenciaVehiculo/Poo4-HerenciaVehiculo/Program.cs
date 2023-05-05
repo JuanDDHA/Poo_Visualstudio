d3ba766d0cdfdd84a81cdfbf74ad89834609d510
Juan D'Dios <132464133+JuanDDHA@users.noreply.github.com>
2023-05-04 21:28:37 -0600

    Add files via upload

---
+++ Poo4-HerenciaVehiculo/Poo4-HerenciaVehiculo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo4_HerenciaVehiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo v = new Vehiculo(1, "halux", "cem", 5000, 40000, 2022);
+            Console.WriteLine("Vehiculo: " + v);
+            Console.WriteLine("\n\n");
+            Auto auto = new Auto(1, "asus", "toto", 10000, 40000, 2020, true);
+            Console.WriteLine("Auto: " + auto);
+            Console.WriteLine("\n\n");
+            Moto moto = new Moto(1, "vento", "suzuki", 3000, 48000, 2022, true);
+            Console.WriteLine("Moto: " + moto);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Poo4-HerenciaVehiculo/Poo4-HerenciaVehiculo/Vehiculo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo4_HerenciaVehiculo
+{
+    class Vehiculo
+    {
+        int id;
+        string marca;
+        string modelo;
+        int km;
+        decimal precio;
+        int año;
+
+        public Vehiculo(int id, string marca, string modelo, int km, decimal precio, int año)
+        {
+            this.id = id;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.km = km;
+            this.precio = precio;
+            this.año = año;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public int Km { get => km; set => km = value; }
+        public decimal Precio { get => precio; set => precio = value; }
+        public int Año { get => año; set => año = value; }
+        public override string ToString()
+        {
+            return "Modelo: " + modelo + "\nMarca: " + marca + "\nPrecio: " + precio + "\nAño: " + año + "\n";
+        }
+        public virtual decimal CalPrecio()
+        {
+            return precio;
+        }
+        
+        
+    }
+    class Auto : Vehiculo
+    {
+        bool airbag;
+        public Auto(int id, string marca, string modelo, int km, decimal precio, int año, bool airbag)
+            : base(id, marca, modelo, km, precio, año)
+        {
+            this.airbag = airbag;
+        }
+
+        public bool Airbag { get => airbag; set => airbag = value; }
+
+        public override decimal CalPrecio()
+        {
+            if (Airbag)
+            {
+                return base.CalPrecio() + 200;
+            }
+            else
+            {
+                return base.CalPrecio();
+            }
+        }
+        public override string ToString()
+        {
+            return "Modelo: " + Modelo + "\nMarca: " + Marca + "\nPrecio: " + CalPrecio() + "\nAño: " + Año + "\n";
+        }
+    }
+    class Moto : Vehiculo
+    {
+        bool sidecar;
+        public Moto(int id, string marca, string modelo, int km, decimal precio, int año, bool sidecar)
+            : base(id, marca, modelo, km, precio, año)
+        {
+            this.sidecar = sidecar;
+        }
+
+        public bool Sidecar { get => sidecar; set => sidecar = value; }
+
+        public override decimal CalPrecio()
+        {
+            if (sidecar)
+            {
+                return base.CalPrecio() + 50;
+            }
+            else
+            {
+                return base.CalPrecio();
+            }
+        }
+        public override string ToString()
+        {
+            return "Modelo: " + Modelo + "\nMarca: " + Marca + "\nPrecio: " + CalPrecio() + "\nAño: " + Año + "\n";
+        }
+    }
+
+}

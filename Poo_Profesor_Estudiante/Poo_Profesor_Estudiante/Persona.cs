81105da32ce360b163c6190355db6597627367f7
Juan D'Dios <132464133+JuanDDHA@users.noreply.github.com>
2023-05-04 22:28:10 -0600

    Add files via upload

---
+++ Poo_Profesor_Estudiante/Poo_Profesor_Estudiante/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Profesor_Estudiante
+{
+    class Persona
+    {
+        string nom;
+
+        public Persona(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public override string ToString()
+        {
+            return "Nombre: " + nom + "\n";
+        }
+
+    }
+    class Estudiante:Persona
+    {
+        public Estudiante(string nom) :base(nom)
+        {
+
+        }
+        public void Estudiar()
+        {
+            Console.WriteLine("El estudiante esta al dia con la clase");
+        }
+    }
+    class Profesor:Persona
+    {
+        public Profesor(string nom):base(nom)
+        {
+
+        }
+        public void Explicar()
+        {
+            Console.WriteLine("El profesor esta explicando la clase");
+        }
+    }
+}
---
+++ Poo_Profesor_Estudiante/Poo_Profesor_Estudiante/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Profesor_Estudiante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] personas = new Persona[3];
+            Profesor per1 = new Profesor("juan");
+            personas[0] = per1;
+            Estudiante per2 = new Estudiante("sara");
+            personas[1] = per2;
+            Estudiante per3 = new Estudiante("Carlos");
+            personas[2] = per3;
+
+            Console.WriteLine("\tImpresion");
+            Console.WriteLine(per1.ToString());
+            per1.Explicar();
+            Console.WriteLine(per2.ToString());
+            per2.Estudiar();
+            Console.WriteLine(per3.ToString());
+            per3.Estudiar();
+
+            Console.ReadKey();
+        }
+    }
+}

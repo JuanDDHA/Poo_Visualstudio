3cfe4bacc84f305ed4188d453014a48364e641ce
Juan D'Dios <132464133+JuanDDHA@users.noreply.github.com>
2023-05-04 21:26:53 -0600

    Add files via upload

---
+++ Poo_Computadora/Poo_Computadora/Computadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Computadora
+{
+    class Computadora
+    {
+        int capdisco;
+        int espciodisponible;
+        bool on = false;
+
+        public Computadora(int capdisco, int espciodisponible, bool on)
+        {
+            this.capdisco = capdisco;
+            this.espciodisponible = espciodisponible;
+            this.on = on;
+        }
+
+        public int Capdisco { get => capdisco; set => capdisco = value; }
+        public int Espciodisponible { get => espciodisponible; set => espciodisponible = value; }
+        public bool On { get => on; set => on = value; }
+        public void AñadirDatos(int datos)
+        {
+            if(on)
+            {
+                if(datos>espciodisponible)
+                {
+                    espciodisponible = 0;
+                }
+                else
+                {
+                    espciodisponible -= datos;
+                }
+            }
+            else
+            {
+                Console.WriteLine("lacomputadora esta apagada");
+            }
+        }
+        public void EliminarDato(int datos)
+        {
+            if(on)
+            {
+                if(datos<espciodisponible)
+                {
+                    espciodisponible += datos;
+                }
+                else
+                {
+                    espciodisponible = capdisco;
+                }
+            }
+        }
+        public void Encender()
+        {
+            on = true;
+            if(on)
+            {
+                Console.WriteLine("Bienvenido");
+            }
+            else
+            {
+                Console.WriteLine("no se ha podido enceder...esta conectada la unidad a la fuente de poder?...intente de nuevo ");
+            }
+            
+        }
+        public void Apagar()
+        {
+            Console.WriteLine("apagando la unidad... espere unos instantes...");
+        }
+
+    }
+}
---
+++ Poo_Computadora/Poo_Computadora/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Computadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computadora computadora = new Computadora(100, 0, true);
+            Console.WriteLine("Desea enceder la pc? (s/n)");
+            string r = Console.ReadLine().ToLower();
+            if(r=="s")
+            {
+                computadora.Encender();
+                Console.WriteLine("ingrese la cantidad de datos a añadir:");
+                int datos = int.Parse(Console.ReadLine());
+                computadora.AñadirDatos(datos);
+                Console.WriteLine("Ingrese la cantidad de datos a eliminar");
+                datos = int.Parse(Console.ReadLine());
+                computadora.EliminarDato(datos);
+                Console.ReadKey();
+            }
+            else
+            {
+                computadora.Apagar();
+                Console.ReadKey();
+            }    
+        }
+    }
+}
